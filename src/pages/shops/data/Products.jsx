5f6ea596e55f9382e9ce5e12ab0bc7e462d43caf
Sujan Mongar <sujanallaymongar506@gmail.com>
2022-02-11 14:22:28 +0600

    Change

--- src/pages/shops/data/Products.jsx
+++ src/pages/shops/data/Products.jsx
@@ -103,6 +103,11 @@ const Products = () => {
                 variant="outlined"
                 disableElevation
                 startIcon={<AddShoppingCartRoundedIcon />}
+                sx={{
+                  borderRadius: 2,
+                  boxShadow:
+                    'rgb(145 158 171 / 20%) 0px 0px 2px 0px, rgb(145 158 171 / 12%) 0px 12px 24px -4px',
+                }}
               >
                 {item.button}
               </Button>
