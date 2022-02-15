8d2988a4d43c182b86931ea552cd0b3f90747dee
Sujan Monger <sujanallaymongar506@gmail.com>
2022-02-15 13:40:18 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/_index.scss';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Shops from './pages/shops/Shops';
 import Carts from './pages/carts/Carts';
@@ -13,7 +13,7 @@ import { theme } from './theme';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route exact path="dashboard" element={<Dashboard />} />
@@ -24,7 +24,7 @@ ReactDOM.render(
           <Route exact path="profile" element={<Profile />} />
         </Route>
       </Routes>
-    </Router>
+    </HashRouter>
   </ThemeProvider>,
   document.getElementById('root')
 );
