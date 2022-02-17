477215ebe991761baaa445c35f6f285b091f7056
Sujan Mongar <99518676+Sujan-Ale@users.noreply.github.com>
2022-02-17 09:37:34 +0600

    Change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/_index.scss';
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Shops from './pages/shops/Shops';
@@ -19,7 +18,7 @@ ReactDOM.render(
           <Route exact path="/dashboard" element={<Dashboard />} />
           <Route exact path="/shops" element={<Shops />}>
             <Route path="/shops/:id" element={<Shops />} />
-              </Route>
+          </Route>
           <Route exact path="/carts" element={<Carts />} />
           <Route exact path="/profile" element={<Profile />} />
         </Route>
@@ -28,8 +27,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
